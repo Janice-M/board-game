6b03aa73208ea3ecce6b4dbad36d30d9e3707ea2
Janice Muia <janiceink001@gmail.com>
2020-04-04 01:01:46 +0300

    property values to be rendered after click

--- src/components/board-box.jsx
+++ src/components/board-box.jsx
@@ -7,7 +7,7 @@ import React from 'react';
 export const Box =(props) => {
     return (
         <button className = "board_box" onClick={props.onClick}>
-            
+            {props.value}
         </button>
 
 
