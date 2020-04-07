20d6fece65b9860f6a750cbf8aa16b172dc66368
Janice Muia <janiceink001@gmail.com>
2020-04-07 20:33:51 +0300

    importations done

---
+++ src/components/board.jsx
@@ -0,0 +1,21 @@
+
+// this is the board that will have all the individual boxes
+//src/componentts/ board.jsx
+
+
+// impoertations dzaddy 
+
+import React from 'react';
+import {link } from 'react-router-dom';
+
+//imporrt storage for all the marked boxes here too
+
+import  { Storage} from ' ./../storage/storage';
+
+// importing the box component
+
+import { Box } from './board-box';
+
+// importing utility functions
+
+import * as utils from '../utils/functions'
