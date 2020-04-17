799cc8f4708149c4d2f14428b97602470d33039f
Janice Muia <janiceink001@gmail.com>
2020-04-17 22:46:53 +0300

    scoreboard logic component

--- src/components/scoreboard.jsx
+++ src/components/scoreboard.jsx
@@ -6,4 +6,16 @@ import { Link } from 'react-router-dom';
 // Import Storage object
 import { Storage } from './../storage/storage';
 
+// create scoreboard component 
 
+export class Scoreboard component extends Reach.component {
+        state = {
+            scoreboard:[]
+        }
+
+        //load data from our local storage to upgrade scores
+            let storage = await new Storage ().getData()
+            this.setState({
+                scoreboard: storage
+            })
+}
