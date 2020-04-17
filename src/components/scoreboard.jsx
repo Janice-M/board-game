ab6862a69751ac0a7d676438b98280078cd52822
Janice Muia <janiceink001@gmail.com>
2020-04-17 22:39:52 +0300

    importations X new branch

---
+++ src/components/scoreboard.jsx
@@ -0,0 +1,9 @@
+// src/components/scoreboard.jsx
+///
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Import Storage object
+import { Storage } from './../storage/storage';
+
+
