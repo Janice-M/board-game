03f0e4e87c49e8e5e092270fdb641e07593a03b2
Janice Muia <janiceink001@gmail.com>
2020-04-18 22:15:38 +0300

    debbugging

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -171,7 +171,8 @@ render()
          </div>
         /* restart the game  */
         {winner && <div className="board-footer">
-                        <button className="btn" onClick={this.handleBoardRestart}>Start new game</button>
+         
+<button className="btn" onClick={this.handleBoardRestart}>Start new game</button> 
         </div>
         </div>
 
