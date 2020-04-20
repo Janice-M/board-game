bcbb7a1608b98bfd2cea503099c28d87003e6525
Janice Muia <janiceink001@gmail.com>
2020-04-20 21:06:16 +0300

    importations on the main app

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+// src/index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// Import Board and Scoreboard views
+import { Board } from './components/board';
+import { Scoreboard } from './components/scoreboard';
+
+import './styles/board.css';
+import './styles/box.css';
+import './styles/buttons.css';
