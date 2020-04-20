0d5c2eede80dceabaafd9c3687a9c0f95c654a15
Janice Muia <janiceink001@gmail.com>
2020-04-20 21:07:57 +0300

    app component in injex.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -10,3 +10,17 @@ import { Scoreboard } from './components/scoreboard';
 import './styles/board.css';
 import './styles/box.css';
 import './styles/buttons.css';
+
+// Create App component
+class App extends React.Component {
+    render() {
+      return (
+        <div className="app">
+          <BrowserRouter>
+            <Route exact path="/" component={Scoreboard}/>
+            <Route path="/board" component={Board}/>
+          </BrowserRouter>
+        </div>
+      )
+    }
+  }
